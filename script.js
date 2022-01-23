18dada56b7e3d9afc3f17dc1e7fdd1686fed06a2
Aseel <aseelalnassiry@gmail.com>
2022-01-23 02:12:20 -0800

    added the pad and a reset button

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const mainContainer = document.querySelector("#main-container");
+const resetButton = document.querySelector("#reset-button");
 let pixelBoxes = [];
 
-for (i = 0; i < 16; i++) {
+for (i = 0; i < 16 * 16; i++) {
   pixelBoxes[i] = document.createElement("div");
   pixelBoxes[i].setAttribute("class", "pixel-box");
   mainContainer.appendChild(pixelBoxes[i]);
 }
+
+pixelBoxes.forEach((pixelBox) => {
+  pixelBox.addEventListener("mouseover", () => {
+    pixelBox.setAttribute("style", "background-color: black");
+  });
+});
+
+resetButton.addEventListener("click", () => {
+  pixelBoxes.forEach((pixelBox) => {
+    pixelBox.setAttribute("style", "background-color: white");
+  });
+});
+
+
