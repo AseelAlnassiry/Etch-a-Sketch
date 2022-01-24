506061fa3cf9a982ad67de08efa615f98681eafe
Aseel <aseelalnassiry@gmail.com>
2022-01-23 19:00:55 -0800

    working color changer, size changer and reset button

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
 const mainContainer = document.querySelector("#main-container");
 const resetButton = document.querySelector("#reset-button");
+const sizeRange = document.querySelector("#size-range");
+const rangeIdentifier = document.querySelector("#range-identifier");
+const colorPicker = document.querySelector("#color-picker");
+var currentColor = "#000000";
 let pixelBoxes = [];
 
-for (i = 0; i < 16 * 16; i++) {
-  pixelBoxes[i] = document.createElement("div");
-  pixelBoxes[i].setAttribute("class", "pixel-box");
-  mainContainer.appendChild(pixelBoxes[i]);
+function sizeBoxes(inputSize) {
+  // Remove the old pixel boxes from the DOM
+  pixelBoxes.forEach((pixelBox) => {
+    pixelBox.remove();
+  });
+
+  // Create new pixel boxes based on the input size and input them into the main container
+  for (i = 0; i < inputSize * inputSize; i++) {
+    pixelBoxes[i] = document.createElement("div");
+    pixelBoxes[i].setAttribute("class", "pixel-box");
+    mainContainer.appendChild(pixelBoxes[i]);
+  }
+
+  //Adjust the width and height of the pixel boxes so they all fit inside the main container
+  var correctDimension = 500.0 / inputSize;
+  pixelBoxes.forEach((pixelBox) => {
+    pixelBox.setAttribute(
+      "style",
+      `width: ${correctDimension}px; height: ${correctDimension}px`
+    );
+  });
+
+  pixelBoxes.forEach((pixelBox) => {
+    pixelBox.addEventListener("mouseover", () => {
+      pixelBox.style.backgroundColor = currentColor;
+    });
+  });
 }
 
-pixelBoxes.forEach((pixelBox) => {
-  pixelBox.addEventListener("mouseover", () => {
-    pixelBox.setAttribute("style", "background-color: black");
-  });
-});
+sizeBoxes(sizeRange.value);
 
 resetButton.addEventListener("click", () => {
   pixelBoxes.forEach((pixelBox) => {
-    pixelBox.setAttribute("style", "background-color: white");
+    pixelBox.style.backgroundColor = "white";
   });
 });
 
+sizeRange.addEventListener("input", () => {
+  sizeBoxes(sizeRange.value);
+  rangeIdentifier.textContent = `${sizeRange.value} x ${sizeRange.value}`;
+});
 
+colorPicker.addEventListener('input', () => {
+  currentColor = colorPicker.value; 
+})
